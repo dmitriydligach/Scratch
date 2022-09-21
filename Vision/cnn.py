0ccad28b0f51c1f1283be5e09dad9f18c8fbb277
Dima <ddligach@luc.edu>
2022-09-21 15:18:17 -0500

    added more layers improving accuracy to 99.3%

--- Vision/cnn.py
+++ Vision/cnn.py
@@ -23,6 +23,9 @@ def get_model():
 
   x = layers.Conv2D(filters=32, kernel_size=3, activation='relu')(inputs)
   x = layers.MaxPooling2D(pool_size=2)(x)
+  x = layers.Conv2D(filters=64, kernel_size=3, activation='relu')(x)
+  x = layers.MaxPooling2D(pool_size=2)(x)
+  x = layers.Conv2D(filters=128, kernel_size=3, activation='relu')(x)
   x = layers.Flatten()(x)
 
   outputs = layers.Dense(10, activation='softmax')(x)
