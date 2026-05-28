f081c106daab120e0499100a5902c2d070cfe7d1
dmitriydligach <ddligach@luc.edu>
2026-05-28 10:58:37 -0500

    cleaned up a little

--- RL/skeleton.py
+++ RL/skeleton.py
@@ -3,35 +3,35 @@ import time
 import gymnasium as gym
 import numpy as np
 
-# this script provides the skeleton for q learning
-# the agent moves randomly in an environment
-# ready to fill in the details of q-learning implementation
+# this script provides a skeleton for implementing q-learning
+# the agent moves randomly in an environment and a more
+# intelligent policy can be implemented, e.g. using q-learning
 
 # Initialize the environment
 env = gym.make("FrozenLake-v1", render_mode="human")
 
 # Reset the environment to generate the first observation
 observation, info = env.reset(seed=42)
-
 print("observation space", env.observation_space)
-print()
 
 state_space = env.observation_space.n
 print("There are ", state_space, " possible states")
 
 action_space = env.action_space.n
-print("There are ", action_space, " possible actions")
-print()
+print("There are ", action_space, " possible actions\n")
 
 for _ in range(1000):
-    # this is where you would insert your policy
+
+    # insert your policy here
+    # ...
+
+    # but for now sample a random action
     action = env.action_space.sample()
 
     # pause
     time.sleep(2)
 
     # step (transition) through the environment with the action
-    # receiving the next observation, reward and if the episode has terminated or truncated
     observation, reward, terminated, truncated, info = env.step(action)
 
     print("action: ", action)
@@ -41,6 +41,7 @@ for _ in range(1000):
 
     # If the episode has ended then we can reset to start a new episode
     if terminated or truncated:
+        print("terminated... starting a new episode\n")
         observation, info = env.reset()
 
 env.close()
