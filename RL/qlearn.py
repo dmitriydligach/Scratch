8b8a6469a76dd8bed0a422e0544964280976b808
dmitriydligach <ddligach@luc.edu>
2026-05-29 11:15:50 -0500

    removed observation

--- RL/qlearn.py
+++ RL/qlearn.py
@@ -9,7 +9,7 @@ import tqdm
 # Implement training and testing
 
 # Training parameters
-n_training_episodes = 100 # 10000
+n_training_episodes = 10000
 lr = 0.7
 
 # Evaluation parameters
@@ -19,7 +19,6 @@ n_eval_episodes = 100  # Total number of test episodes
 env_id = "FrozenLake-v1"  # Name of the environment
 max_steps = 99  # Max steps per episode
 gamma = 0.95  # Discounting rate
-eval_seed = []  # The evaluation seed of the environment
 
 # Exploration parameters
 max_epsilon = 1.0  # Exploration probability at start
@@ -27,21 +26,17 @@ min_epsilon = 0.05  # Minimum exploration probability
 decay_rate = 0.0005  # Exponential decay rate for exploration prob
 
 def initialize_q_table(state_space, action_space):
-    """Initialize the Q table"""
+    """Initialize the Q table to zeros"""
 
-    qtable = np.zeros((state_space, action_space))
-
-    return qtable
+    return np.zeros((state_space, action_space))
 
 def greedy_policy(qtable, state):
-    "Exploitation: take the action with the highest Q value"""
+    "Return action with the highest Q value"""
 
-    action = np.argmax(qtable[state][:])
-
-    return action
+    return np.argmax(qtable[state][:])
 
 def epsilon_greedy_policy(qtable, state, epsilon):
-    """Exploitation: take the action with the highest Q value"""
+    """Exploration with probability epsilon"""
 
     # Randomly generate a number between 0 and 1
     random_num = random.uniform(0, 1)
@@ -64,10 +59,12 @@ def train(n_training_episodes,
           q):
     """Train the Q table"""
 
-    for episode in tqdm.tqdm(range(n_training_episodes)):
+    for episode in range(n_training_episodes):
+        print("\nepisode: ", episode)
 
         # reduce epsilon (because we need less and less exploration)
         epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+        print("epsilon", epsilon, "\n")
 
         # reset the environment
         state, info = env.reset()
@@ -78,15 +75,23 @@ def train(n_training_episodes,
 
         # repeat
         for step in range(max_steps):
+
             # choose the action At using epsilon greedy policy
             action = epsilon_greedy_policy(q, state, epsilon)
 
             # take the action and observe Rt+1 and St+1
             new_state, reward, terminated, truncated, info = env.step(action)
 
-            # sarsamax / q-learning using temporal differences
+            print("action: ", action)
+            print("new state: ", new_state)
+            print("reward: ", reward, "\n")
+
+            # sarsamax / q-learning with temporal differences
             q[state][action] = q[state][action] + \
                 lr * (reward + gamma * np.max(q[new_state]) - q[state][action])
+
+            if reward > 0:
+                print(q)
 
             if terminated or truncated:
                 break
@@ -119,7 +124,7 @@ def evaluate():
 if __name__ == "__main__":
 
     # initialize the environment
-    env = gym.make("FrozenLake-v1", render_mode="human")
+    env = gym.make(env_id, render_mode="human")
 
     # reset the environment to generate the first observation
     observation, info = env.reset(seed=42)
@@ -132,18 +137,17 @@ if __name__ == "__main__":
     print("There are ", action_space, " possible actions\n")
 
     qtable = initialize_q_table(state_space, action_space)
-    print("Q table before training:\n", qtable)
 
-    # qtable = train(
-    #     n_training_episodes,
-    #     min_epsilon,
-    #     max_epsilon,
-    #     decay_rate,
-    #     env,
-    #     max_steps,
-    #     qtable)
-    # print("Q table after training:\n", qtable)
+    qtable = train(
+        n_training_episodes,
+        min_epsilon,
+        max_epsilon,
+        decay_rate,
+        env,
+        max_steps,
+        qtable)
+    print("Q table after training:\n", qtable)
 
-    evaluate()
+    # evaluate()
 
     env.close()
